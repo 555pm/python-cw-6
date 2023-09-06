d2adc87d762829dcb492104bb8becccecbe6a147
Sae <142683788+555pm@users.noreply.github.com>
2023-09-06 19:55:49 +0300

    cw6.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # write your code here
+person = {
+    "name": "sarah",
+    "age": "17",
+    "hobbies":['drawing', 'singing', 'dancing', 'taking pics']
+}
+print(person["name"])
+print(person["age"])
+
+person["age"] = "20"
+person["contry"]= "Kuwait"
+print(person)
+print(len(person))
+
+person["hobbies"].append('shopping')
+print(person)
+
+def check_hobbies(person):
+    if "hobbies" > "3":
+        print("WOW YOU ARE AMAZING")
+    else:
+        print("get off your phone")
+check_hobbies(person)
+
